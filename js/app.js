2d52be0cd6682e7fed9ca593693b8372db2847c5
XXX <vitya_maksimov_22@mail.ru>
2024-12-10 02:29:30 +0700

    Added button state control using isExpanded and displayedCards variables

--- js/app.js
+++ js/app.js
@@ -18,3 +18,28 @@ const swiperText = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next'
 	}
 });
+
+let isExpanded = false; // Флаг для отслеживания состояния кнопки
+let displayedCards = []; // Массив для хранения показанных карточек
+
+document.getElementById("loadMore").addEventListener("click", () => {
+  const hiddenCards = document.querySelectorAll('.card.hidden');
+
+  if (!isExpanded) {
+    for (let i = 0; i < 2 && i < hiddenCards.length; i++) {
+      hiddenCards[i].classList.remove('hidden');
+      displayedCards.push(hiddenCards[i]);
+    }
+        
+    this.innerText = 'Показать меньше';
+    isExpanded = true;
+  } else {
+    for (let card of displayedCards) {
+      card.classList.add('hidden');
+    }
+        
+    displayedCards = [];
+    this.innerText = 'Показать все';
+    isExpanded = false;
+  }
+});
